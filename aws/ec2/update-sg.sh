1077071bbc8486085cbd74723e4e9fe197d4f06f
g-six <gerard.echiverri@gmail.com>
2019-09-28 15:26:02 +0800

    aws ec2 secgroup script

--- aws/ec2/update-sg.sh
+++ aws/ec2/update-sg.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 HEADER=$(cat ./header.txt)
 
+# Color definitions
+CYAN='\033[0;36m'
+LCYAN='\033[1;36m'
+LPURP='\033[1;35m'
+ORANGE='\033[0;33m'
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+# Emojis
+ROCKET='\xf0\x9f\x9a\x80'
+
+
 case $2 in
   me)
     CIDR=$(curl -s ifconfig.co)'/32';
@@ -14,12 +27,19 @@ SERVICE='ec2'
 COMMAND=''
 CMD="aws ${SERVICE}"
 OPTS="--group-name ${3}"
+DESCRIPTION="Description=${4}"
+
+if [ $DESCRIPTION = "Description=" ]; then
+  printf "${RED}\nYou need to provide a description for IP: ${CIDR}${NC}\n\n"
+  exit
+fi
+
 case $1 in
   add)
     COMMAND='authorize-security-group-ingress'
     OPTS=$OPTS' --ip-permissions'
     OPTS=$OPTS' FromPort=22,ToPort=22,IpProtocol=tcp,IpRanges=['
-    OPTS=$OPTS"'"'{CidrIp='$CIDR',Description='$4"'}]"
+    OPTS=$OPTS"'"'{CidrIp='$CIDR','$DESCRIPTION"'}]"
     ;;
   remove)
     COMMAND='revoke-security-group-ingress'
@@ -33,14 +53,6 @@ esac
 
 CMD="${CMD} ${COMMAND} ${OPTS}"
 
-CYAN='\033[0;36m'
-LCYAN='\033[1;36m'
-LPURP='\033[1;35m'
-ORANGE='\033[0;33m'
-RED='\033[0;31m'
-YELLOW='\033[1;33m'
-NC='\033[0m' # No Color
-
 printf "${RED}"
 printf "$HEADER"
 printf "${NC}\n\n"
@@ -52,6 +64,6 @@ printf "\n  One moment please...."
 eval "$CMD"
 
 printf "\n\n  ${LCYAN}Done!${NC}"
-echo '\xf0\x9f\x9a\x80'
+echo $ROCKET
 echo '\n'
 

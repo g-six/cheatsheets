ddfbaafb58716704a8fe2d988e3d389d5ee51d11
Gerard Rey Echiverri <gerard@idearobin.com>
2019-09-30 10:26:14 +0800

    refactor: remove --profile, use AWS_DEFAULT_PROFILE

--- aws/ec2/cancel-spot.sh
+++ aws/ec2/cancel-spot.sh
@@ -1,3 +1,2 @@
 aws ec2 cancel-spot-instance-requests \
-  --profile $AWS_PROFILE \
   --spot-instance-request-ids $SPOT_REQ_ID
--- aws/ec2/terminate.sh
+++ aws/ec2/terminate.sh
@@ -1,3 +1,2 @@
 aws ec2 terminate-instances \
-  --profile $AWS_PROFILE \
   --instance-ids $EC2_ID

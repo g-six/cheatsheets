c627a2329f584445fd24d084681b80e4eff0c9d1
g-six <gerard@idearobin.com>
2019-10-02 09:08:41 +0800

    refactor: describe-instances list-images

--- aws/ec2/describe-instances.sh
+++ aws/ec2/describe-instances.sh
@@ -1,2 +1,19 @@
-aws ec2 describe-instances \
-  --instance-ids $EC2_ID
+output='--output '$2
+query='--query '$1
+if [ -z $1 ]
+then
+  query=""
+fi
+if [ -z $2 ]
+then
+  output=""
+fi
+instances=$(aws ec2 describe-instances \
+  --instance-ids $EC2_ID \
+  $output $query)
+echo $instances | json_pp 
+exit 1
+for instance in $instances
+do
+  echo $instance',\n'
+done
--- aws/ec2/list-images.sh
+++ aws/ec2/list-images.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-images=$(aws ec2 describe-images --owner self --query Images[*].[ImageId,Name] --output text)
+filter='--filter Name=name,Values='$1
+if [ -z $1 ]
+then
+  filter=''
+fi
+
+output='--output '$2
+if [ -z $2 ]
+then
+  output=''
+fi
+
+
+images=$(aws ec2 describe-images --owner self --query Images[*].[ImageId,Name] $filter $output)
 Ymd=$(date +'%Y%m%d')
 
 echo $images

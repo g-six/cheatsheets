7e8c92b2da2273249929e167ab624f1c898add08
Gerard Rey Echiverri <gerard@idearobin.com>
2019-10-09 17:37:43 +0800

    ci: update launch.sh

--- aws/ec2/launch.sh
+++ aws/ec2/launch.sh
@@ -39,12 +39,38 @@ if [ -z $EC2_KEY ] ; then
   key=$(cat ~/.ssh/id_rsa.pub | xargs)
 
   echo '#!/bin/bash' > raw.txt
-  raw='echo "'$key'" > /home/idearobin/.ssh/authorized_keys'
 
+  raw='echo "'$key'" > /home/idearobin/.ssh/authorized_keys'
   echo $raw >> raw.txt
 
   raw='echo "%cardinals ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/90-cloud-init-users'
   echo $raw >> raw.txt
+
+  raw='curl ifconfig.co > /etc/hostname'
+  echo $raw >> raw.txt
+
+  raw='adduser --disabled-password --gecos "" '`whoami`
+  echo $raw >> raw.txt
+
+  raw='usermod -aG docker '`whoami`
+  echo $raw >> raw.txt
+  raw='usermod -aG cardinals '`whoami`
+  echo $raw >> raw.txt
+  raw='usermod -aG sudo'`whoami`
+  echo $raw >> raw.txt
+
+  raw='mkdir /home/'`whoami`'/.ssh'
+  echo $raw >> raw.txt
+
+  raw='echo "'$key'" > /home/'`whoami`'/.ssh/authorized_keys'
+  echo $raw >> raw.txt
+
+  raw='chown -R '`whoami`':'`whoami`' /home/'`whoami`'/.ssh'
+  echo $raw >> raw.txt
+
+  raw='chmod 600 /home/'`whoami`'/.ssh/authorized_keys'
+  echo $raw >> raw.txt
+
   export USER_DATA=$(cat raw.txt | base64)
 
   printf "Attach user data:\n$USER_DATA\n\n"
